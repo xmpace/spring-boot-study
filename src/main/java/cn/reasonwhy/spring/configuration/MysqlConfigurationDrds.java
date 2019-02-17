c98658c2d059f787b0ea294f68ff80454ea7efa2
xiaomi <xmpace@gmail.com>
2019-02-18 00:09:37 +0800

    datasource config

--- src/main/java/cn/reasonwhy/spring/configuration/MysqlConfigurationDrds.java
+++ src/main/java/cn/reasonwhy/spring/configuration/MysqlConfigurationDrds.java
@@ -2,15 +2,19 @@ package cn.reasonwhy.spring.configuration;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.support.DefaultBeanNameGenerator;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(DataSourceProperties.class)
 @MapperScan(
         value = "cn.reasonwhy.spring.rds.mapper",
         sqlSessionFactoryRef = "drdsSqlSessionFactory",
@@ -20,8 +24,8 @@ public class MysqlConfigurationDrds {
 
     @Bean
     @ConfigurationProperties(prefix = "spring.druid.drds")
-    protected DruidDataSource drdsDruidDataSource() {
-        return DruidDataSourceBuilder.create().build();
+    protected DruidDataSource drdsDruidDataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().type(DruidDataSource.class).build();
     }
 
     @Bean
